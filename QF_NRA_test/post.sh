6bda2ba04927298fbdef0a25906df9eb77d8ebdb
hrcarryu <495958672@qq.com>
2022-04-29 16:38:37 +0800

    final

--- QF_NRA_test/post.sh
+++ QF_NRA_test/post.sh
@@ -4,6 +4,7 @@ function post_process() {
     for file in $@/*;do 
         sed -i -r 's/_[0-9]+\//\//g' $file
         sed -i -r 's/temp\///g' $file
+        sed -i -r 's/_split//g' $file
     done
 }
 
