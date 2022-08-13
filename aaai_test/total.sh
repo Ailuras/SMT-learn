b9969f52dee9623a19a8c26a3fe184933930bc58
hrcarryu <495958672@qq.com>
2022-08-13 13:45:18 +0800

    aaai

--- aaai_test/total.sh
+++ aaai_test/total.sh
@@ -1,28 +1,29 @@
+
 echo hhh
 echo -----------------------------
-python count.py hhh_classify_60/
+python count.py hhh_classify_20/
 echo -----------------------------
 echo cvc5
 echo -----------------------------
-python count.py cvc5_classify_60/
+python count.py cvc5_classify_20/
 echo -----------------------------
 echo yices2
 echo -----------------------------
-python count.py yices2_classify_60/
+python count.py yices2_classify_20/
 echo -----------------------------
 echo z3
 echo -----------------------------
-python count.py z3_classify_60/
+python count.py z3_classify_20/
 echo -----------------------------
 echo "z3(b)"
 echo -----------------------------
-python count.py z3\(b\)_classify_60/
+python count.py z3\(b\)_classify_20/
 echo -----------------------------
 echo mathsat
 echo -----------------------------
-python count.py mathsat_classify_60/
+python count.py mathsat_classify_20/
 echo -----------------------------
 echo aprove
 echo -----------------------------
-python count.py aprove_classify_60/
+python count.py aprove_classify_20/
 echo -----------------------------
